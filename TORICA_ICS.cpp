36002d3143c34c4e93a548b4179f958a566eb21a
771-8bit <kazuki.fujita8823@gmail.com>
2023-05-26 12:30:20 +0900

    Update TORICA_ICS.cpp

--- TORICA_ICS.cpp
+++ TORICA_ICS.cpp
@@ -1,15 +1,19 @@
 #include "TORICA_ICS.h"
 #include <Arduino.h>
 
-int TORICA_ICS::read_Angle() {
-  if (serial->available() >= 3) {
+int TORICA_ICS::read_Angle()
+{
+  int retval = 0;
+  while (serial->available() >= 3)
+  {
     char c = (char)serial->read();
-    if (c == 128) {
+    if (c == 128)
+    {
       uint16_t bit1 = (char)serial->read();
       uint16_t bit2 = (char)serial->read();
       angle = bit1 * 128 + bit2;
-      return angle;
+      retval = angle;
     }
   }
-  return 0;
+  return retval;
 }
